6727debb52d7f05dca89c8ca8017b0b7e80a4a00
filecc <fil.gia@live.it>
2023-11-16 15:48:08 +0100

    Refactor admin dashboard with updated links and post sorting

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,31 +8,18 @@ const path = require("path");
  */
 
 function index(req, res) {
-  let html = fs.readFileSync(path.resolve("./views/dashboard.html"), "utf8");
+  const linksYesUser = [
+    { href: "/", label: "Home"},
+    { href: "/posts", label: "Posts" },
+    { href: "/admin", label: "Dashboard", active: true},
+    { href: "/logout", label: "Logout"},
+  ];
 
-  html = html.replace("{{ admin }}", req.cookies.user.username);
   const posts = fs.readFileSync(path.resolve("./db/posts.json"), "utf8");
   const postList = JSON.parse(posts).sort((a, b) => b.id - a.id)
 
-    .map((post) => {
-      return `<li style="display: flex; align-items: center; gap: 1rem">
-    <span>ID: ${post.id}</span>
-    <a href="/posts/${post.id}">${post.title}</a>
-    <form action="/admin/edit/${post.id}" method="POST">
-    <input type="hidden" name="id" value="${post.id}">
-    <input style='margin: 1rem 0;' type="submit" value="Modifica">   
-    </form>
-    <form action="/api/delete/${post.id}" method="POST">
-    <input type="hidden" name="id" value="${post.id}">
-    <input style='margin: 1rem 0;' type="submit" value="Cancella">   
-    </form>
-    
-    </li>`;
-    })
-    .join("");
-  html = html.replace("{{ postList }}", postList);
-
-  res.send(html);
+  res.render("dashboard", { user: req.cookies.user, links: linksYesUser, posts: postList });
+  return
 }
 
 function edit(req, res){
